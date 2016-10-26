54047107d6fa5b89738cf28e8be3472aaaa09138
Peli de Halleux <jhalleux@microsoft.com>
2016-10-26 11:08:20 -0700

    support for basic file operations (append, remove)

--- libs/files/files.cpp
+++ libs/files/files.cpp
@@ -7,17 +7,19 @@
 /**
 * File system operations
 */
-//% weight=5
+//% weight=5 color=#002050
 namespace files {
     /**
     * Appends text and a new line to a file
+    * @param filename file name, eg: "output.txt"
+    * @param text the string to append to the end of the file
     */
-    //% blockId="files_append_line" block="file %name|append string %text"
-    //% blockExternalInputs=1
-    void appendLine(StringData* name, StringData* text) {
+    //% blockId="files_append_line" block="file %filename|append string %text"
+    //% blockExternalInputs=1 weight=90 blockGap=8
+    void appendLine(StringData* filename, StringData* text) {
         if (!text) return;
 
-        MicroBitFile f(name);
+        MicroBitFile f(filename);
         f.append(text);
         f.append("\r\n");
         f.close();
@@ -25,14 +27,27 @@ namespace files {
 
     /**
     * Appends text and a new line to a file
+    * @param filename file name, eg: "output.txt"
+    * @param text the string to append to the end of the file
     */
-    //% blockId="fs_append_line" block="file %name|append line %text"
-    //% blockExternalInputs=1
-    void appendString(StringData* name, StringData* text) {
+    //% blockId="fs_append_string" block="file %filename|append line %text"
+    //% blockExternalInputs=1 weight=86 blockGap=8
+    void appendString(StringData* filename, StringData* text) {
         if (!text) return;
 
-        MicroBitFile f(name);
+        MicroBitFile f(filename);
         f.append(text);
         f.close();
+    }
+
+    /**
+    * Removes the file. There is no undo for this operation.
+    * @param filename name of the file to remove, eg: "output.txt"
+    */
+    //% blockId="fs_remove" block="file remove %filename"
+    //% weight=50
+    void remove(StringData* filename) {
+        MicroBitFile f(filename);
+        f.remove();        
     }
 }
