ab0447666790d1ca5bf361bea174e08d40c9b667
Peli de Halleux <jhalleux@microsoft.com>
2016-12-16 17:18:11 -0800

    annotating APIs to support onstart

---
+++ libs/core/basic.ts
@@ -0,0 +1,14 @@
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=#0078D7 weight=100
+namespace basic {
+    /**
+     * Code that runs when the program starts.
+     */
+    //% blockId=pxtStart block="on start" weight=10
+    export function onStart(handler: Action) {
+        // this is just a stub, should not be used
+        handler();
+    }
+}
