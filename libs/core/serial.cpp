a3f7fd0f91c0a70f33d8c2cbcde1b5cee078dc67
Owen Brotherwood <OwenBrotherwood@users.noreply.github.com>
2017-03-26 19:40:58 +0200

    comma correction and remove trailing space
    That will teach me just to do cut and paste via github web interface :(

--- libs/core/serial.cpp
+++ libs/core/serial.cpp
@@ -20,21 +20,21 @@ enum BaudRate {
   //% block=38400
   BaudRate38400 = 38400,
   //% block=28800
-  BaudRate28800 = 28800,    
+  BaudRate28800 = 28800,
   //% block=19200
-  BaudRate19200 = 19200,    
+  BaudRate19200 = 19200,
   //% block=14400
-  BaudRate14400 = 14400,    
+  BaudRate14400 = 14400,
   //% block=9600
-  BaudRate9600 = 9600
+  BaudRate9600 = 9600,
   //% block=4800
-  BaudRate4800 = 4800,    
+  BaudRate4800 = 4800,
   //% block=2400
-  BaudRate2400 = 2400,    
+  BaudRate2400 = 2400,
   //% block=1200
-  BaudRate1200 = 1200,    
+  BaudRate1200 = 1200,
   //% block=300
-  BaudRate300 = 300,    
+  BaudRate300 = 300
 };
 
 enum Delimiters {
