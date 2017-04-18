a9b34497672a670f6edd673387be36bd72449c25
Peli de Halleux <jhalleux@microsoft.com>
2017-04-18 09:51:52 -0700

    inject file importers as extensions

---
+++ editor/extension.ts
@@ -0,0 +1,27 @@
+/// <reference path="../node_modules/pxt-core/built/pxteditor.d.ts" />
+
+namespace pxt.editor {
+    export function initExtensionsAsync(opts?: pxt.editor.ExtensionOptions): Promise<pxt.editor.ExtensionResult> {
+        const res: pxt.editor.ExtensionResult = {
+            hexFileImporters: [{
+                id: "blockly",
+                canImport: data => data.meta.cloudId == "microbit.co.uk" && data.meta.editor == "blockly",
+                importAsync: (project, data) => project.createProjectAsync({
+                    filesOverride: {
+                        "main.blocks": data.source
+                    }, name: data.meta.name
+                })
+            }, {
+                    id: "td",
+                    canImport: data => data.meta.cloudId == "microbit.co.uk" && data.meta.editor == "touchdevelop",
+                    importAsync: (project, data) =>
+                        project.createProjectAsync({
+                            filesOverride: { "main.blocks": "", "main.ts": "  " },
+                            name: data.meta.name
+                        }).then(() => project.convertTouchDevelopToTypeScriptAsync(data.source))
+                        .then(text => this.textEditor.overrideFile(text))
+                }]
+        };
+        return Promise.resolve<pxt.editor.ExtensionResult>(res);
+    }
+}
