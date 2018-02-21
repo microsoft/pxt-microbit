05a4621ec8cd686ff21bf00afa1765dd7a84ba46
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-02-21 05:46:22 -0800

    moving sendbuffer to radio

--- libs/core/pins.ts
+++ libs/core/pins.ts
@@ -61,14 +61,6 @@ namespace pins {
         }
         return 0;
     }
-
-    /**
-     * Sends an encoded color buffer to a strip of
-     * WS2812 color LEDs.
-     */
-    //% shim=ws2812SendBufferAsm
-    export function ws2812SendBuffer(buf: Buffer, pin: DigitalPin) {
-    }    
 }
 
 
