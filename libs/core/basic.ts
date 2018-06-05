bc734607fd1ab985744daa707c4efcf3f144018f
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-06-04 22:16:28 -0700

    fixng typing error

--- libs/core/basic.ts
+++ libs/core/basic.ts
@@ -10,6 +10,6 @@ namespace basic {
     //% async
     //% parts="ledmatrix" interval.defl=150
     export function showNumber(value: number, interval?: number) {
-        showString(Math.roundWithPrecision(value, 2), interval);
+        showString(Math.roundWithPrecision(value, 2).toString(), interval);
     }
 }
