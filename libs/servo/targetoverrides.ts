72307e824e10aa29e2f43f60c297b75288fb78f7
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-10-04 16:58:55 -0700

    adding whenUsed

--- libs/servo/targetoverrides.ts
+++ libs/servo/targetoverrides.ts
@@ -1,8 +1,8 @@
 namespace servos {
-    //% block="servo P0" fixedInstance
+    //% block="servo P0" fixedInstance whenUsed
     export const P0 = new servos.PinServo(pins.P0);
-    //% block="servo P1" fixedInstance
+    //% block="servo P1" fixedInstance whenUsed
     export const P1 = new servos.PinServo(pins.P1);
-    //% block="servo P2" fixedInstance
+    //% block="servo P2" fixedInstance whenUsed
     export const P2 = new servos.PinServo(pins.P2);
 }
