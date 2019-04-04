cac3f87a6ec183184293c4305a0980481fbfcae0
Joseph Wunderlich <jwunderl@uw.edu>
2019-04-04 13:54:13 -0700

    remove macros that were moved to common packages

--- libs/core/platform.h
+++ libs/core/platform.h
@@ -1,28 +1,3 @@
 // helpful define to handle C++ differences in package
 #define PXT_MICROBIT_TAGGED_INT 1
-
-// cross version compatible way of access data field
-#ifndef PXT_BUFFER_DATA
-#define PXT_BUFFER_DATA(buffer) buffer->data
-#endif
-
-// cross version compatible way of access data length
-#ifndef PXT_BUFFER_LENGTH
-#define PXT_BUFFER_LENGTH(buffer) buffer->length
-#endif
-
-#ifndef PXT_CREATE_BUFFER
-#define PXT_CREATE_BUFFER(data, len) pxt::mkBuffer(data, len)
-#endif
-
-// cross version compatible way of accessing string data
-#ifndef PXT_STRING_DATA
-#define PXT_STRING_DATA(str) str->getUTF8Data()
-#endif
-
-// cross version compatible way of accessing string length
-#ifndef PXT_STRING_DATA_LENGTH
-#define PXT_STRING_DATA_LENGTH(str) str->getUTF8Size()
-#endif
-
 #define PXT_POWI 1
