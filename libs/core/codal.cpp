4681660f1cfac9a75bbb8671204b4293e8a5e79c
Michal Moskal <michal@moskal.me>
2020-04-01 08:59:01 -0700

    Revert "V1 handlers (#1851)"
    This reverts commit 99d7e47791863eca2ef60416de067275ab50c1cb.

--- libs/core/codal.cpp
+++ libs/core/codal.cpp
@@ -43,29 +43,7 @@ struct FreeList {
     FreeList *next;
 };
 
-void dispatchBackground(MicroBitEvent e, void *action) {
-    lastEvent = e;
-    auto value = fromInt(e.value);
-    runAction1((Action)action, value);
-}
-
-void dispatchForeground(MicroBitEvent e, void *action) {
-    lastEvent = e;
-    auto value = fromInt(e.value);
-    runAction1((Action)action, value);
-}
-
-void deleteListener(MicroBitListener *l) {
-    if (l->cb_param == (void (*)(MicroBitEvent, void *))dispatchBackground ||
-        l->cb_param == (void (*)(MicroBitEvent, void *))dispatchForeground) {
-        decr((Action)(l->cb_arg));
-        unregisterGCPtr((Action)(l->cb_arg));
-    }
-}
-
 static void initCodal() {
-    uBit.messageBus.setListenerDeletionCallback(deleteListener);
-
     // repeat error 4 times and restart as needed
     microbit_panic_timeout(4);
 }
@@ -76,25 +54,27 @@ void dumpDmesg() {}
 // An adapter for the API expected by the run-time.
 // ---------------------------------------------------------------------------
 
-static bool backgroundHandlerFlag = false;
-void setBackgroundHandlerFlag() {
-    backgroundHandlerFlag = true;
+// We have the invariant that if [dispatchEvent] is registered against the DAL
+// for a given event, then [handlersMap] contains a valid entry for that
+// event.
+void dispatchEvent(MicroBitEvent e) {
+    lastEvent = e;
+
+    auto curr = findBinding(e.source, e.value);
+    auto value = fromInt(e.value);
+    if (curr)
+        runAction1(curr->action, value);
+
+    curr = findBinding(e.source, DEVICE_EVT_ANY);
+    if (curr)
+        runAction1(curr->action, value);
 }
 
 void registerWithDal(int id, int event, Action a, int flags) {
-    if (backgroundHandlerFlag) {
-        uBit.messageBus.listen(id, event, dispatchBackground, a);
-        backgroundHandlerFlag = false;
-    } else {
-        uBit.messageBus.ignore(id, event, dispatchForeground);
-        uBit.messageBus.listen(id, event, dispatchForeground, a);
-    }
-    incr(a);
-    registerGCPtr(a);
-}
-
-void unregisterFromDal(void *a) {
-    uBit.messageBus.ignore(MICROBIT_EVT_ANY, MICROBIT_EVT_ANY, dispatchBackground, a);
+    // first time?
+    if (!findBinding(id, event))
+        uBit.messageBus.listen(id, event, dispatchEvent, flags);
+    setBinding(id, event, a);
 }
 
 void fiberDone(void *a) {
