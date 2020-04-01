177a210b3d3398b64980d0daa5cc94492bb8700f
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-03-31 18:57:36 -0700

    Update libs/core/pins.cpp
    Co-Authored-By: Michał Moskal <michal@moskal.me>

--- libs/core/pins.cpp
+++ libs/core/pins.cpp
@@ -313,7 +313,7 @@ namespace pins {
     }
 
     /**
-    * Sets the volume the pitch pin
+    * Sets the volume on the pitch pin
     * @param volume the intensity of the sound from 0..255
     */
     //% blockId=device_analog_set_pitch_volume block="analog set pitch volume $volume"
