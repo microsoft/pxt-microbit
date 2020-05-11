84253332f1d6bce1a159d3ac2cf69f794a97d59d
Peli <jhalleux@microsoft.com>
2020-05-10 23:06:00 -0700

    add various control apis

--- libs/core/control.cpp
+++ libs/core/control.cpp
@@ -290,6 +290,15 @@ namespace control {
         MicroBitEvent evt(src, value, (MicroBitEventLaunchMode)mode);
     }
 
+
+    /**
+    * Allocates the next user notification event
+    */
+    //% help=control/allocate-notify-event
+    int allocateNotifyEvent() {
+        return pxt::allocateNotifyEvent();
+    }
+
     /**
      * Registers an event handler.
      */
