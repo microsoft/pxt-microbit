9f6e885aff24fdf5e170328c6f24e4e55a8d777e
Peli de Halleux <peli@Pelis-MacBook-Pro.local>
2020-10-22 09:54:52 +0200

    wiring up project in default template

---
+++ libs/microphone/microphone.cpp
@@ -0,0 +1,77 @@
+#include "pxt.h"
+#include "LevelDetector.h"
+#include "LevelDetectorSPL.h"
+
+#define MICROPHONE_MIN 52.0f
+#define MICROPHONE_MAX 120.0f
+
+namespace pxt {
+#if MICROBIT_CODAL
+    codal::LevelDetectorSPL* getMicrophoneLevel();
+#endif   
+}
+
+namespace input {
+/**
+* Registers an event that runs when a loud sound is detected
+*/
+//% help=input/on-loud-sound
+//% blockId=input_on_loud_sound block="on loud sound"
+//% parts="microphone"
+//% weight=88 blockGap=12
+void onLoudSound(Action handler) {
+#if MICROBIT_CODAL
+    pxt::getMicrophoneLevel(); // wake up service
+    registerWithDal(DEVICE_ID_MICROPHONE, LEVEL_THRESHOLD_HIGH, handler);
+#else
+    target_panic(PANIC_VARIANT_NOT_SUPPORTED);
+#endif    
+}
+
+/**
+* Reads the loudness through the microphone from 0 (silent) to 255 (loud)
+*/
+//% help=input/sound-level
+//% blockId=device_get_sound_level block="sound level"
+//% parts="microphone"
+//% weight=34 blockGap=8
+int soundLevel() {
+#if MICROBIT_CODAL
+    auto level = pxt::getMicrophoneLevel();
+    if (NULL == level)
+        return 0;
+    const int micValue = level->getValue();
+    const int scaled = max(MICROPHONE_MIN, min(micValue, MICROPHONE_MAX)) - MICROPHONE_MIN;
+    return min(0xff, scaled * 0xff / (MICROPHONE_MAX - MICROPHONE_MIN));
+#else
+    target_panic(PANIC_VARIANT_NOT_SUPPORTED);
+    return 0;
+#endif    
+}
+
+/**
+* Sets the minimum threshold for a loud sound
+*/
+//% help=input/set-loud-sound-threshold
+//% blockId=input_set_loud_sound_threshold block="set loud sound threshold %value"
+//% parts="microphone"
+//% value.min=1 value.max=255
+//% group="More" weight=14 blockGap=8
+void setLoudSoundThreshold(int value) {
+#if MICROBIT_CODAL
+    auto level = pxt::getMicrophoneLevel();
+    if (NULL == level)
+        return;
+
+    threshold = max(0, min(0xff, threshold));
+    const int scaled = MICROPHONE_MIN + threshold * (MICROPHONE_MAX - MICROPHONE_MIN) / 0xff;
+    if (sound == SoundThreshold::Loud)
+        level->setHighThreshold(scaled);
+    else
+        level->setLowThreshold(scaled);
+#else
+    target_panic(PANIC_VARIANT_NOT_SUPPORTED);
+#endif
+}
+
+}
