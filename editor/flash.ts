c5b5c45f2b7bdcea18c947e1a75c6f36a31739c3
peli <pelikhan@users.noreply.github.com>
2020-10-27 01:47:50 -0700

    more logging

--- editor/flash.ts
+++ editor/flash.ts
@@ -225,7 +225,7 @@ class DAPWrapper implements pxt.packetio.PacketIOWrapper {
                     const end = Math.min(hexArray.length, offset + chunkSize);
                     const nextPage = hexArray.slice(offset, end);
                     nextPage.unshift(nextPage.length);
-                    log(`next page [${offset.toString(16)}, ${end.toString(16)}] (${hexArray.length - end}kb left)`)
+                    log(`next page [${offset.toString(16)}, ${end.toString(16)}] (${((hexArray.length - end) / 1000) | 0}b left)`)
                     return this.cmsisdap.cmdNums(0x8C /* DAPLinkFlash.WRITE */, nextPage)
                         .then(() => {
                             if (!aborted && end < hexArray.length) {
