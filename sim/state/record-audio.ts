a72c6c77d00401851fd3713e84d81b1d55dd79b8
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2023-04-17 16:40:09 -0700

    A start to the recording logic

--- sim/state/record-audio.ts
+++ sim/state/record-audio.ts
@@ -5,12 +5,53 @@ namespace pxsim  {
 }
 namespace pxsim.record {
 
+    class AudioRecording {
+        stream: MediaStream;
+    }
+    
+    function init() {
+        
+    }
+
+    function initRecording() {
+        navigator.mediaDevices.getUserMedia({video: false, audio:true});
+    }
+
     export async function record(): Promise<void> {
         //request permission is asynchronous
         let b = board();
         if (!b.recordingState.currentlyRecording) {
             b.recordingState.currentlyRecording = true;
             runtime.queueDisplayUpdate();
+            const context = new AudioContext();
+            const stream = await navigator.mediaDevices.getUserMedia({video: false, audio:true});
+            const source1 = context.createMediaStreamSource(stream);
+            const recorder = new MediaRecorder(stream);
+            recorder.start(1000);
+            let chunks: Blob[] = [];
+
+            recorder.onstop = (e: BlobEvent) => {
+                const audio = document.createElement("audio");
+                const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+                chunks = []
+                const audioURL = URL.createObjectURL(blob);
+                audio.src = audioURL;
+            }
+            // recorder.stop();
+            recorder.ondataavailable = (e: BlobEvent) => {
+                chunks.push(e.data);
+            }
+            // source1.connect(context.destination);
+
+
+
+            // const buffer = context.createBuffer(1, 22050, 44100);
+            // const source = context.createBufferSource();
+            // source.buffer = buffer;
+            // source.connect(context.destination);
+            // source1.start();
+            // stream.getAudioTracks();
+
         }
     }
 
