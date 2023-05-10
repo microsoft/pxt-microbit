712dc67282d03c588a3e0d16bc6570409c5c8b3e
Thomas Sparks <thsparks@microsoft.com>
2023-05-10 16:18:18 -0700

    Update non-deprecated melody block

--- libs/core/music.ts
+++ libs/core/music.ts
@@ -321,7 +321,7 @@ namespace music {
 
     /**
      * Gets the melody array of a built-in melody.
-     * @param melody the note name, eg: Note.C
+     * @param name the note name, eg: Note.C
      */
     //% weight=50 help=music/builtin-melody
     //% blockId=device_builtin_melody block="%melody"
@@ -334,7 +334,7 @@ namespace music {
 
     /**
      * Gets the melody array of a built-in melody.
-     * @param name the note name, eg: Note.C
+     * @param melody the melody name
      */
     //% weight=60 help=music/builtin-melody
     //% blockId=device_builtin_melody_playable block="%melody"
