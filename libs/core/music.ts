28ff5e1ae2c8a274940f6c0f7172386b0aa51123
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2023-05-10 16:19:39 -0700

    Apply suggestions from code review
    Co-authored-by: Richard Knoll <riknoll@users.noreply.github.com>
    Co-authored-by: Kim Ying <15070078+kimprice@users.noreply.github.com>

--- libs/core/music.ts
+++ libs/core/music.ts
@@ -337,7 +337,7 @@ namespace music {
      * @param name the note name, eg: Note.C
      */
     //% weight=60 help=music/builtin-melody
-    //% blockId=device_builtin_melody_playable block="%melody"
+    //% blockId=device_builtin_melody_playable block="$melody"
     //% toolboxParent=music_playable_play
     //% toolboxParentArgument=toPlay
     //% duplicateShadowOnDrag
