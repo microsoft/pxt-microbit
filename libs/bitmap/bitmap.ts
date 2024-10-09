2a92c15f601196daa7ee59d375b07904066ee0ac
Tom Ball <tball@microsoft.com>
2024-10-09 14:24:56 -0700

    remove fixedInstasnce

--- libs/bitmap/bitmap.ts
+++ libs/bitmap/bitmap.ts
@@ -29,7 +29,6 @@ namespace bitmap {
 
 //% snippet='bmp` `'
 //% pySnippet='bmp(""" """)'
-//% fixedInstances
 interface Bitmap {
     /**
      * Draw an icon (monochromatic image) using given color
