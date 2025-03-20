ab49ff5b51b9a9dfbe6badb13285e38acf9c2d0d
Tom Ball <tball@microsoft.com>
2025-03-20 08:48:53 -0700

    add sim functions

---
+++ sim/state/audio-samples.ts
@@ -0,0 +1,14 @@
+namespace pxsim.samples {
+
+    export function enable(): void {
+        // TODO
+    }
+
+    export function setSampleRate(src: number, sampleRate: number): void {
+        // TODO
+    }
+
+    export function playAsync(src: number, buf: Buffer): void {
+        // TODO
+    }
+}
