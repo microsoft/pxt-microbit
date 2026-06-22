68a1fb0fa6e11c873bfe7123b32a8a5b086261cb
Richard Knoll <riknoll@microsoft.com>
2026-06-22 11:53:36 -0700

    add aria label for beat fractions

--- libs/core/music.ts
+++ libs/core/music.ts
@@ -113,12 +113,16 @@ enum BeatFraction {
     //% block=1
     Whole = 1,
     //% block="1/2"
+    //% ariaLabel="half"
     Half = 2,
     //% block="1/4"
+    //% ariaLabel="quarter"
     Quarter = 4,
     //% block="1/8"
+    //% ariaLabel="eighth"
     Eighth = 8,
     //% block="1/16"
+    //% ariaLabel="sixteenth"
     Sixteenth = 16,
     //% block="2"
     Double = 32,
